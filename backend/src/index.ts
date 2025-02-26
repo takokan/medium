521bc71c37b32f95b027277d8f4c82db82d7095d
takokan <takokan.dev@gmail.com>
2025-02-27 01:01:20 +0530

    added api endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,34 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/extension';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { sign, verify } from 'hono/jwt';
 
 const app = new Hono<{
     Bindings: {
-        DATABASE_URL: string
+        DATABASE_URL: string,
+        JWT_SECRET: string
     }
 }>()
+
+app.use('/api/v1/blog/*', async (c, next) => {
+    const header = c.req.header("authorization") || "";
+    const token = header.split(" ")[1];
+    
+    const response = await verify(token, c.env.JWT_SECRET);
+    if(response){
+        next()
+    } else {
+        c.status(403);
+        return c.json({error: "unauthorized"})
+    }
+})
 
 // endpoint to get the blogs with id
 app.get('/api/v1/blog/:id', (c) => {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
+
     return c.text("/api/v1/blog")
 });
 
@@ -27,17 +43,39 @@ app.post('/api/v1/signup', async (c) => {
     }).$extends(withAccelerate())
 
     const body = await c.req.json();
-    await prisma.user.create({
+    const user = await prisma.user.create({
         data: {
             email: body.email,
             password: body.password,
         },
     })
-    return c.text("/api/v1/signup")
+    const secret = c.env.JWT_SECRET;
+    const token = sign({id: user.id}, secret);
+
+    return c.json({
+        jwt: token
+    });
 });
 
-app.post('/api/v1/signin', (c) => {
-    return c.text("/api/v1/signin")
+app.post('/api/v1/signin', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+    const user = prisma.user.findUnique({
+        where: {
+            email: body.email
+        }
+    });
+
+    if(!user){
+        c.status(403);
+        return c.json({error: "User not found"})
+    }
+
+    const jwt = await sign({id: user.id}, c.env.JWT_SECRET);
+    return c.json({jwt})
 });
 
 
